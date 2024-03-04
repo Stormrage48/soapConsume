97009f9320ef462b6c60f5191548a32745be672d
f.albay <f.albay@cekino.com>
2024-03-04 17:47:40 +0300

    Initial commit

---
+++ src/test/java/com/examle/soapConsume/SoapConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examle.soapConsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
